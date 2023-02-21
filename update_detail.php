69338d372de2cdef9b72aa135317c11c281f7afd
P21_0065 <vvsevensquare>
2023-02-21 12:44:37 +0530

    Init

---
+++ update_detail.php
@@ -0,0 +1,219 @@
+
+	 
+Gitlab Login Access
+===================
+Username : vvsevensquare
+Password : seven2@1234
+
+pc password = klgn*up2
+
+Keka username : virsing.v@upsquare.in	
+password : nf2rGx-35vQ
+
+
+git config user.virsingvasava "veervasava"
+git config credential.username "nodejs@123"
+
+	
+Database 
+=======
+username : virsing
+Password : Virsing@99 
+	
+Username: bpostgres
+postgres@123123
+
+/* delete forlder command */
+	- sudo rm -rf countmee-courier gf
+
+/* git clone for specific branch */
+	- sudo git clone -b dev_stagging  https://gitlab.com/devsevensquare/countmee-courier.git
+
+/* git clone */
+
+- sudo  git clone  https://gitlab.com/devsevensquare/countmee-courier.git
+	
+/* git clone command *
+	- git add --all
+	- git status
+	- git checkout
+	- git status
+	- git commit -m " Stripe Connect"
+	- git pull
+	- git push 
+	
+	- git config --global user.email "your_email"
+
+/* git config --global user.email "vvsevensquare" */
+
+
+/* This  command is use for check version */
+	- ubuntu version  = lsb_release -a,
+	- php  = php -version , php -v,
+	- composer = composer -v,
+	
+/* Use for hidden files show  */
+	- ctrl + h
+	
+	
+	
+/* This for folder permision command we can  use */
+	- sudo chmod -Rf  777 countmee-courier
+	- sudo chmod -R 775 storage bootstrap/cache
+	- sudo chmod 777 -R storage bootstrap/cache
+
+/* This command is use for install php 8.0.12 version */
+	- sudo chmod 755 xampp-linux-x64-8.0.12-0-installer.run
+	- sudo ./xampp-linux-x64-8.0.12-0-installer.run
+
+/* This one command use for install php 7.4.25  */
+	- sudo chmod 755 xampp-linux-x64-7.4.27-1-installer.run
+	- sudo ./xampp-linux-x64-7.4.27-1-installer.run
+
+/* Here reference link for php install  some  command */
+   - https://www.codegrepper.com/code-examples/shell/install+php+8+ubuntu
+
+/* php removed */
+	- sudo apt-get purge php8.*
+
+/* if xampp is not start then u can perform this command  */
+- sudo /opt/lampp/lampp start
+- sudo /etc/init.d/apache2 stop   
+- sudo /opt/lampp/lampp start
+- sudo service mysql stop
+- sudo /opt/lampp/lampp restart
+
+- sudo systemctl stop nginx
+- sudo /etc/init.d/apache2 stop
+- sudo /etc/init.d/mysql stop
+- sudo /etc/init.d/proftpd stop
+- sudo /opt/lampp/lampp restart
+
+
+sudo /opt/lampp/lampp start
+sudo /etc/init.d/apache2 stop   
+sudo /opt/lampp/lampp start
+sudo service mysql stop
+sudo /opt/lampp/lampp restart
+
+sudo systemctl stop nginx
+sudo /etc/init.d/apache2 stop
+sudo /etc/init.d/mysql stop
+sudo /etc/init.d/proftpd stop
+sudo /opt/lampp/lampp restart
+	
+
+ Postgres sql 
+ ==========
+
+Username: postgres
+password: postgres@123123
+reference :  https://www.liquidweb.com/kb/change-a-password-for-postgresql-on-linux-via-command-line/
+
+/* 
+apt-get install php7.4-pgsql
+DB Connection :- https://www.titanwolf.org/Network/q/5c8da207-990e-40cb-a798-bf400570492c/y
+	
+ /* uninstall */ 
+ => sudo apt-get --purge remove postgresql*
+ 
+ /*node js removed*/
+ => sudo apt-get remove nodejs npm node*
+
+
+
+
+sudo -u postgres psql
+ALTER USER postgres PASSWORD 'postgres@123'
+
+the email is admin@silentdonor.com
+the password is Scale123$
+
+
+git clone https://gitlab.com/devsevensquare/match-up.git
+sudo chmod 777 -R storage bootstrap/cache
+
+composer self-update 1.6.3
+
+password = stripepasswordisqwerty
+
+php artisan make:migration create_docs_table
+
+php artisan make:job SendEmailOrganisationUsers
+php artisan make:mail SendEmailOrganizeUsers
+
+sudo update-alternatives --config php
+
+https://github.com/googleapis/google-cloud-php/issues/3545
+
+	sudo /opt/lampp/lampp start
+	sudo /etc/init.d/apache2 stop   
+	sudo /opt/lampp/lampp start
+	
+ https://askphpquestions.com/2021/11/17/grpc-installed-successfully-on-ubuntu-but-php-is-looking-for-it-in-another-folder/
+	
+ rm /usr/local/bin/composer
+
+php composer-setup.php --version=1.6.3-alpha8
+composer self-update 1.6.3
+
+
+Admin
+======
+url = https://isea.norigenglobal.com/login
+Username: admin@isea.com
+Password: Isea@123
+
+
+Organisation
+==========
+URL : Login
+Your E.mail : virsing+33@sevensquaretech.com
+Password : lwl2sRfSzP
+
+Users
+======
+E.mail : virsing+34@sevensquaretech.com
+Password : H6fI6Ygmm1
+
+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
+
+
+Daily Report
+=============
+Project Name = Tejarh
+=================
+ 
+Progress report for  05/12/2022
+======================
+Today's Covered Points :
+ 
+Both Flow users and business 
+
+=> Items review and rating module @done
+=> After take pull on server migrate bug fixed @done 
+=> Business user review and rating module @ in-progress 
+=>  Business user review Review Module html integartion done @done
+    
+
+Note: Both flow users and business flow 
+ => Review Module html integartion done @done
+ => Filter bugs 
+In progress Task:
+ 
+Thanks,
+Virsing Vasava
+
+
+
+
+
+
+
+
+
+
+
+
+
+
