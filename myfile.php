b6569568d250f7930ecc5f49c2dc004c0078f049
P21_0065 <vvsevensquare>
2023-02-28 11:58:25 +0530

    Add file

---
+++ myfile.php
@@ -0,0 +1,42 @@
+
+Pan
+====
+AXOPV3736H
+Adhar
+=======
+527636253115
+
+Blood Group "A"
+“Simplicity is the soul of efficiency.” – Austin Freeman
+
+“The computer was born to solve problems that did not exist before.” – Bill Gates 
+Strength 
+Flexibility
+Versatility
+Creativity
+
+PHP, Laravel, JQuery, Javascript,NodeJS, WP
+H-17/179 Nidhi Apartment, Shastrinagar, Naranpura Ahmedabad - 380063
+
+PHP
+Laravel
+JQuery
+Javascript
+NodeJS
+SQL 
+
+Vacation planning (Get Out and Go)
+Reading
+Learning a new language
+
+Food
+==========
+Punjab and Gujarati Dish 
+
+Color
+======
+White and Light Blue 
+
+Role Model 
+===========
+Warren Buffett 
