54d0a96a1f9ce8671417b6b9fb66ae5d8234657e
P21_0065 <vvsevensquare>
2023-02-28 14:45:07 +0530

    File

---
+++ File.php
@@ -0,0 +1,10 @@
+28 Feb 2023
+
+Hello HR / Sir,
+
+I am writing to inform you that today ( 28 February 2023 ) is my last day at Seven Square Technosoft as  Sr.PHP Developer role.
+Not only was it a wonderful learning experience for me, but I also had a very good time working with the team during my work tenure. 
+I want to thank you for your support. I hope our paths cross again.
+
+Sincerely,
+Virsing Vasava 
